b3c95fb2818cb647bcc3377ce3c208f649384bd9
Dom <415.dominic.smith@gmail.com>
2015-05-14 07:39:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ document.onload = function addElement(){
 		if (i % 2 === 0){
 			var newDiv = document.createElement('div');
 			newDiv.style.backgroundColor = 'red';
-  		newDiv.style.width = '11.1%';
-  		newDiv.style.float = 'left';
-  		newDiv.style.paddingBottom = '11.1%';
+  			newDiv.style.width = '11.1%';
+  			newDiv.style.float = 'left';
+  			newDiv.style.paddingBottom = '11.1%';
 			document.body.appendChild(newDiv);
 		}else {
 			var newDiv = document.createElement('div');
 			newDiv.style.backgroundColor = 'black';
-  		newDiv.style.width = '11.1%';
-  		newDiv.style.float = 'left';
-  		newDiv.style.paddingBottom = '11.1%';
+  			newDiv.style.width = '11.1%';
+  			newDiv.style.float = 'left';
+  			newDiv.style.paddingBottom = '11.1%';
 			document.body.appendChild(newDiv);
 		}	
 	}
